672f102f674c12712f4b8cb3d36087b008184855
esther-kpet <dev@estherkpetemey.com>
2026-01-16 12:11:32 +0000

    Second commit

---
+++ src/main/java/estherkpetemey/com/sa/controller/TestController.java
@@ -0,0 +1,15 @@
+package estherkpetemey.com.sa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path= "test")
+public class TestController {
+
+    @GetMapping(path="string")
+    public String getString(){
+        return "Chaine de caractère transmise par SA";
+    }
+}
